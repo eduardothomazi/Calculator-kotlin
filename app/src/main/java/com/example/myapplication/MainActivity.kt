1220f80f518c2de7938ad331afe6c4d7fd7ecbdc
Eduardo Thomazi <eduardothomazi91@gmail.com>
2022-07-10 18:54:37 -0300

    adding decimal function and boolean verifications

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,18 +9,33 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private var tvInput : TextView? = null
+    private var tvInput: TextView? = null
+    private var lastNumeric: Boolean = false
+    private var lastDot: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tvInput = findViewById(R.id.tv_input)
     }
-    fun onDigit(view: View){
+
+    fun onDigit(view: View) {
+        lastNumeric = true
         tvInput?.append((view as Button).text)
     }
 
-    fun onClear(view: View){
+    fun onClear(view: View) {
+        lastNumeric = false
         tvInput?.text = ""
+    }
+
+    fun onDecimal(view: View) {
+        if (tvInput?.text?.contains(".") == false) {
+            if (lastNumeric) {
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+            }
+        }
+
     }
 }
